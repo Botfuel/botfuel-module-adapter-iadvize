552e5979bf8c6281df2191fad777a3bec7c38042
Kevin Depuydt <kevin@botfuel.io>
2018-10-10 10:46:36 +0200

    fix export

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const IadvizeAdapter = require('./build/adapters/iadvize-adapter');
 const TransferAction = require('./build/transfer-action');
-const CloseAction = require('./build/stop-action');
+const CloseAction = require('./build/close-action');
 
 module.exports = {
   botfuelModuleRoot: 'build',
